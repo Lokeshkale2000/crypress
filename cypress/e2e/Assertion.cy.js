d5accf7363372b17bed4db41ea872042ffc5db32
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2025-03-17 13:36:55 +0530

    second

--- cypress/e2e/Assertion.cy.js
+++ cypress/e2e/Assertion.cy.js
@@ -3,19 +3,48 @@ describe("Assertion demo", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get("input[placehoder='Username']").type("Admin");
+
+    // and should
+    // 1 way of wiriting code 
+    cy.url().should('include', 'orangehrmlive.com')
+    cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.url().should('contain', 'orangehrm')
+    
+    // 2 way of wiriting of code
+
+      cy.url().should("include", "orangehrmlive.com").should(
+        "eq",
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    ).should("contain", "orangehrm");
+    
+
+
+    // 3 way of wiriting of code
+     cy.url()
+       .should("include", "orangehrmlive.com")
+       .and(
+         "eq",
+         "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+       )
+       .and("contain", "orangehrm");
+  });
+});
+// explicit assertion
+describe("Assertion demo", () => {
+  it("explicit assertion", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("input[placeholder='Username']").type("Admin");
     cy.get("input[placeholder='Password']").type("admin123");
     cy.get("button[type='submit']").click();
-    let expName = "xzy";
+    let expName = "Paul Colling";
 
-    cy.get(".oxd-userdropdown-name").then((x)=> {
+    cy.get(".oxd-userdropdown-name").then((x) => {
+      let actName = x.text();
 
-
-      let actName = x.text()
-      
-      expect(actName).to.equal(expName)
-      expect(actName).to.noual(expName);
-
+      expect(actName).to.equal(expName);
+      expect(actName).to.not.equal(expName);
     });
   });
 });
